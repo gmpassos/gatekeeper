5731465e0f356c17bbbcb6833db8a9dd635e65e6
gmpassos <gracilianomp@gmail.com>
2025-10-04 17:35:11 -0300

    gatekeeper_abuse_test.dart

--- test/gatekeeper_abuse_test.dart
+++ test/gatekeeper_abuse_test.dart
@@ -6,6 +6,8 @@ const ip2 = '192.168.0.2';
 const ip3 = '192.168.0.3';
 
 const period30sec = Duration(seconds: 30);
+const period30min = Duration(minutes: 30);
+const period1hour = Duration(hours: 1);
 
 void main() {
   group('GatekeeperAbuse', () {
@@ -29,7 +31,7 @@ void main() {
 
       expect(
         abuse.computeAccessState(ip1, period30sec),
-        equals(AccessState(ip1, IPState.normal, rate: 1)),
+        equals(AccessState(ip1, IPState.normal, rate: 1, maxAccess: 301)),
       );
 
       ////////////
@@ -44,7 +46,7 @@ void main() {
 
       expect(
         abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 11000),
-        equals(AccessState(ip1, IPState.normal, rate: 27.27)),
+        equals(AccessState(ip1, IPState.normal, rate: 27.27, maxAccess: 301)),
       );
 
       ////////////
@@ -59,12 +61,12 @@ void main() {
 
       expect(
         abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 11000),
-        equals(AccessState(ip1, IPState.normal, rate: 27.27)),
+        equals(AccessState(ip1, IPState.normal, rate: 27.27, maxAccess: 301)),
       );
 
       expect(
         abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 21000),
-        equals(AccessState(ip1, IPState.normal, rate: 28.57)),
+        equals(AccessState(ip1, IPState.normal, rate: 28.57, maxAccess: 301)),
       );
 
       ////////////
@@ -81,22 +83,56 @@ void main() {
 
       expect(
         abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 11000),
-        equals(AccessState(ip1, IPState.normal, rate: 27.27)),
+        equals(AccessState(ip1, IPState.normal, rate: 27.27, maxAccess: 301)),
       );
 
       expect(
         abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 21000),
-        equals(AccessState(ip1, IPState.normal, rate: 28.57)),
+        equals(AccessState(ip1, IPState.normal, rate: 28.57, maxAccess: 301)),
       );
 
       expect(
         abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 31009),
-        equals(AccessState(ip1, IPState.normal, rate: 116.09)),
+        equals(AccessState(ip1, IPState.normal, rate: 116.09, maxAccess: 301)),
       );
+
+      ////////////
+
+      for (var i = 1; i <= 10; ++i) {
+        for (var j = 0; j < 100; ++j) {
+          abuse.notifyAccess(ip1, time: 31009 + (i * 1000) + j);
+        }
+      }
+
+      expect(abuse.accessEvents.length, equals(1121));
+      expect(abuse.accessEvents.init(), equals(1000));
+      expect(abuse.accessEvents.end(), equals(41108));
+
+      expect(
+        abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 31009),
+        equals(AccessState(ip1, IPState.normal, rate: 116.09, maxAccess: 301)),
+      );
+
+      expect(
+        abuse.computeAccessState(ip1, period30sec, initTime: 1, endTime: 41108),
+        equals(AccessState(ip1, IPState.blocked, rate: 817.37, maxAccess: 301)),
+      );
+
+      ////////////
 
       expect(
         abuse.computeMaxAccessPer(period30sec, LoginState(ip1, 0)),
         inInclusiveRange(301, 302),
+      );
+
+      expect(
+        abuse.computeMaxAccessPer(period30min, LoginState(ip1, 0)),
+        inInclusiveRange(18100, 18101),
+      );
+
+      expect(
+        abuse.computeMaxAccessPer(period1hour, LoginState(ip1, 0)),
+        inInclusiveRange(36200, 36201),
       );
     });
   });
