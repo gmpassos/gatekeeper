1c8a5e033d53d936e979a6393d7f5184de93926a
gmpassos <gracilianomp@gmail.com>
2025-10-04 17:51:21 -0300

    docs

--- lib/src/gatekeeper_abuse.dart
+++ lib/src/gatekeeper_abuse.dart
@@ -1,21 +1,37 @@
 import 'package:collection/collection.dart';
 
+/// Represents types of tracked events.
 enum EventType {
+  /// Normal access event.
   access,
+
+  /// Resource not found.
   notFound,
+
+  /// Access denied or invalid.
   badAccess,
+
+  /// Successful login.
   login,
 }
 
+/// Represents a single tracked event.
 class Event {
+  /// Client IP address.
   final String ip;
+
+  /// Type of event.
   final EventType type;
+
+  /// Event timestamp (ms since epoch).
   final int time;
 
+  /// Logged info: may be a user or a bool.
   final Object? logged;
 
   const Event(this.ip, this.type, this.time, this.logged);
 
+  /// True if the event is from a logged user.
   bool get isLogged {
     final logged = this.logged;
     if (logged == null) {
@@ -26,12 +42,14 @@ class Event {
     }
   }
 
+  /// Returns the logged user if available.
   Object? get loggedUser {
     final logged = this.logged;
     if (logged == null || logged is bool) return null;
     return logged;
   }
 
+  /// Returns a new event with modified fields.
   Event copyWith({String? ip, EventType? type, int? time, Object? logged}) =>
       Event(ip ?? this.ip, type ?? this.type, time ?? this.time,
           logged ?? this.logged);
@@ -53,31 +71,40 @@ class Event {
   String toString() => 'Event[$ip]{type: $type, logged: $logged}@$time';
 }
 
+/// Iterable helpers for [Event].
 extension IterableEventExtension on Iterable<Event> {
+  /// Filters by IP.
   Iterable<Event> withIP(String ip) => where((e) => e.ip == ip);
 
+  /// Filters by type.
   Iterable<Event> withType(EventType type) => where((e) => e.type == type);
 
+  /// Filters logged events.
   Iterable<Event> logged() => where((e) => e.isLogged);
 
+  /// Returns all logged users (non-null).
   Iterable<Object> loggedUsers() => map((e) => e.loggedUser).nonNulls;
 
+  /// Filters events after [initTime].
   Iterable<Event> after(Object initTime) {
     final initTimeMs = initTime.toTimeMS();
     return where((e) => e.time >= initTimeMs);
   }
 
+  /// Filters events before [endTime].
   Iterable<Event> before(Object endTime) {
     final endTimeMs = endTime.toTimeMS();
     return where((e) => e.time < endTimeMs);
   }
 
+  /// Filters events between [initTime] and [endTime].
   Iterable<Event> between(Object initTime, Object endTime) {
     final initTimeMs = initTime.toTimeMS();
     final endTimeMs = endTime.toTimeMS();
     return where((e) => e.time >= initTimeMs && e.time < endTimeMs);
   }
 
+  /// Filters using optional start/end times.
   Iterable<Event> filterTime({Object? initTime, Object? endTime}) {
     if (initTime != null) {
       if (endTime != null) {
@@ -92,10 +119,13 @@ extension IterableEventExtension on Iterable<Event> {
     }
   }
 
+  /// First event time.
   int? init() => firstOrNull?.time;
 
+  /// Last event time.
   int? end() => lastOrNull?.time;
 
+  /// Total time span in ms.
   int? period({Object? initTime, Object? endTime}) {
     if (initTime != null) {
       if (endTime != null) {
@@ -121,6 +151,7 @@ extension IterableEventExtension on Iterable<Event> {
     return period;
   }
 
+  /// Rate per [timeMs] in the selected interval.
   num ratePer(int timeMs, {Object? initTime, Object? endTime}) {
     final l = filterTime(initTime: initTime, endTime: endTime).toList();
     final p = l.period(initTime: initTime, endTime: endTime);
@@ -129,16 +160,21 @@ extension IterableEventExtension on Iterable<Event> {
   }
 }
 
-enum IPState {
-  untracked,
-  normal,
-  blocked;
-}
+/// Possible IP states.
+enum IPState { untracked, normal, blocked }
 
+/// Access analysis result.
 class AccessState {
+  /// Client IP.
   final String ip;
+
+  /// Current IP state.
   final IPState state;
+
+  /// Access rate.
   final num? rate;
+
+  /// Maximum allowed accesses.
   final num? maxAccess;
 
   const AccessState(this.ip, this.state, {this.rate, this.maxAccess});
@@ -161,8 +197,12 @@ class AccessState {
       'AccessState[$ip]{state: $state, rate: $rate, maxAccess: $maxAccess}';
 }
 
+/// Login analysis result.
 class LoginState {
+  /// Client IP.
   final String ip;
+
+  /// Count of distinct logged users.
   final int loggedUsers;
 
   const LoginState(this.ip, this.loggedUsers);
@@ -182,8 +222,12 @@ class LoginState {
   String toString() => 'LoginState[$ip]{loggedUsers: $loggedUsers}';
 }
 
+/// Detects abuse based on access and login rates.
 class GatekeeperAbuse {
+  /// Max access per hour for single user.
   final int userMaxAccessPerHour;
+
+  /// Max access multiplier per logged user.
   final int maxAccessUserMultiplier;
 
   GatekeeperAbuse({
@@ -194,11 +238,14 @@ class GatekeeperAbuse {
 
   final QueueList<Event> _accessEvents = QueueList();
 
+  /// Read-only access events.
   List<Event> get accessEvents => UnmodifiableListView(_accessEvents);
 
+  /// Records an access event.
   void notifyAccess(String ip, {bool? logged, Object? time}) => _notifyEvent(
       ip, EventType.access, time?.toTimeMS(), logged, _accessEvents);
 
+  /// Computes IP access state.
   AccessState computeAccessState(String ip, Duration period,
       {Object? initTime, Object? endTime}) {
     final ipEvents = _accessEvents
@@ -214,22 +261,22 @@ class GatekeeperAbuse {
 
     final loginState =
         computeLoginState(ip, initTime: initTime, endTime: endTime);
-
     final maxAccess = computeMaxAccessPer(period, loginState);
 
     if (rate > maxAccess) {
       return AccessState(ip, IPState.blocked, rate: rate, maxAccess: maxAccess);
     }
-
     return AccessState(ip, IPState.normal, rate: rate, maxAccess: maxAccess);
   }
 
+  /// Computes maximum allowed per hour.
   num computeMaxAccessPerHour(LoginState loginState) {
     final users = loginState.loggedUsers;
     if (users <= 0) return userMaxAccessPerHour;
     return userMaxAccessPerHour * maxAccessUserMultiplier * users;
   }
 
+  /// Computes maximum allowed per [period].
   num computeMaxAccessPer(Duration? period, LoginState loginState) {
     var max = computeMaxAccessPerHour(loginState);
     if (period == null) return max;
@@ -239,11 +286,14 @@ class GatekeeperAbuse {
 
   final QueueList<Event> _loginEvents = QueueList();
 
+  /// Read-only login events.
   List<Event> get loginEvents => UnmodifiableListView(_loginEvents);
 
+  /// Records a login event.
   void notifyLogin(String ip, Object user, {Object? time}) =>
       _notifyEvent(ip, EventType.login, time?.toTimeMS(), user, _loginEvents);
 
+  /// Computes login state for an IP.
   LoginState computeLoginState(String ip, {Object? initTime, Object? endTime}) {
     final ipEvents = _loginEvents
         .withIP(ip)
@@ -261,6 +311,7 @@ class GatekeeperAbuse {
   }
 }
 
+/// Time conversion helper.
 extension on Object {
   int toTimeMS() {
     final self = this;
@@ -274,7 +325,9 @@ extension on Object {
   }
 }
 
+/// Numeric comparison helper.
 extension on num? {
+  /// True if within [delta].
   bool eq(num? other, num delta) {
     final self = this;
 
